1cdfb959ee91f2462af2c8e508b9beaf79e01365
Nail Khanipov <foxflameless@gmail.com>
2021-09-03 01:10:46 +0300

    chore: Update tutorials to lastest engine (#1)
    * chore(tutorial): migrate tutorial modules with engine package movement
    
    * chore(tutorial): migrate components to Gestaltv7

--- src/main/java/org/terasology/tutorialMultiplayerExtras/systems/TutorialSystem.java
+++ src/main/java/org/terasology/tutorialMultiplayerExtras/systems/TutorialSystem.java
@@ -16,14 +16,14 @@
 
 package org.terasology.tutorialMultiplayerExtras.systems;
 
-import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.event.ReceiveEvent;
-import org.terasology.entitySystem.systems.BaseComponentSystem;
-import org.terasology.entitySystem.systems.RegisterMode;
-import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.logic.common.ActivateEvent;
-import org.terasology.logic.console.Console;
-import org.terasology.registry.In;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
+import org.terasology.engine.entitySystem.systems.RegisterMode;
+import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.logic.common.ActivateEvent;
+import org.terasology.engine.logic.console.Console;
+import org.terasology.engine.registry.In;
 import org.terasology.tutorialMultiplayerExtras.components.NetComponent;
 import org.terasology.tutorialMultiplayerExtras.events.ConsoleEvent;
 
